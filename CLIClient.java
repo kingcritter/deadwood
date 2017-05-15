c448dcd36ee452e4beff5c0f68e0e70191c66cc4
medemam <medemam@wwu.edu>
2017-05-15 16:40:22 -0700

    Update CLIClient.java

--- CLIClient.java
+++ CLIClient.java
@@ -11,14 +11,16 @@ public class CLIClient {
     //modify game rules
     //distribute initial cards
     //while running
-      //while player turn
+      //while player turn (should we have a variable that tracks player or just go through gameboard?)
         //show possible commands (need variables to track remaining possible actions
         //get command from player
         //check if command is valid
         //if 'who'
-          //get player and role(if any)
+          //get player(should we return a number?)
+          //get role if player is in one
         //if 'where'
-          //get location and scene(if any)
+          //get current room
+          //get scene if room has one
         //if 'move room'
           //attempt to move rooms
         //if 'work part'
